5da8f58c39858a09aafba5bd5d6564e2a41321fe
Derek Jackson <d4jackson84@gmail.com>
2021-04-28 18:16:40 -0400

    added 3/4 extended challenges

--- src/script.js
+++ src/script.js
@@ -45,4 +45,49 @@ function fight(player1, player2, player1Health, player2Health){
     }
 
 }
-fight("Coronavirus", "Humanity", 100, 100);
+// fight("Coronavirus", "Humanity", 100, 100);
+
+// Extended challenges
+// so square
+function square(width){
+    for (let i=0; i < width; i ++){
+        console.log("#".repeat(width));
+    }
+}
+
+// square(6)
+
+// this one makes a triangle.
+function triangle(width){
+    for (let i=width; i <= width; i --){
+        console.log("#".repeat(i));
+        if (i == 0){
+            break;
+        }
+        
+    }
+}
+// triangle(4)
+
+
+// this here also be trianglin'
+function triangle(width){
+    for (let i= 1; i <= width; i ++){
+        console.log("#".repeat(i));
+        if (i >= width){
+            break;
+        }
+        
+    }
+}
+
+// triangle(4)
+
+// git gud: grades edition
+function getGrade(num){
+    return num < 60 ? "F" : num < 70 ? "D" : num < 80 ? "C" : num < 90 ? "B" : num <= 100 ? "A" : "In the immortal words of Taylor Swift, 'You need to calm down.'"
+}
+
+// console.log(getGrade(95))
+
+
